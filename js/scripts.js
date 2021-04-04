2a9a69722f03b3a907866248b6ab20a7bee65594
fumi23 <78896035+fumi1623@users.noreply.github.com>
2021-04-04 18:19:26 +0900

    [add]link

--- js/scripts.js
+++ js/scripts.js
@@ -8,4 +8,21 @@
     $('#box1-text').fadeOut('slow');
   }
 
+  document.getElementById('box2').onclick = function() {
+    $('#box2-text').fadeIn('slow');
+  }
+
+  document.getElementById('X-btn-2').onclick = function() {
+    $('#box2-text').fadeOut('slow');
+  }
+
+  document.getElementById('box3').onclick = function() {
+    $('#box3-text').fadeIn('slow');
+  }
+
+  document.getElementById('X-btn-3').onclick = function() {
+    $('#box3-text').fadeOut('slow');
+  }
+
+  
 }
